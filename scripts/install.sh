3b52a458a51e37e8c516247735e433afbee9756a
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-04-20 20:35:45 +0300

    ci: add missing pytest plugins

--- scripts/install.sh
+++ scripts/install.sh
@@ -29,7 +29,7 @@ for file in requirements.txt requirements-dev.txt requirements-test.txt dev-requ
     fi
 done
 
-pip3 install pytest-json-report pytest-monitor
+pip3 install pytest-json-report pytest-monitor pytest-cov pytest-env pytest-rerunfailures pytest-socket pytest-django
 
 # Install pythonmop (assuming it's in a sibling directory named 'mop-with-dynapt')
 cd ../mop-with-dynapt || exit
