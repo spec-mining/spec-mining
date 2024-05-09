daab793f6064c5c4cab2bf76a9056b750b8042bf
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-05-09 09:10:45 +0300

    fix: parse reports fails to decode result line

--- scripts/parse-reports.py
+++ scripts/parse-reports.py
@@ -93,19 +93,21 @@ def get_num_violations_from_json(projectname, algorithm):
 
 def get_result_line(filename):
     # get last line from file
-    last_line = subprocess.check_output(
-        ['tail', '-1', filename]).decode('utf-8').strip()
-
-    # if the last line is not the one we want, get the last 100 lines and search
-    if 'in' not in last_line or "passed" not in last_line:
-        last_lines = subprocess.check_output(
-            ['tail', '-500', filename]).decode('utf-8').strip().split('\n')
-        for line in reversed(last_lines):
-            if 'in' in line and "passed" in line:
-                last_line = line
-                return last_line
-    else:
-        return last_line
+    try:
+        last_line = subprocess.check_output(
+            ['tail', '-1', filename]).decode('utf-8').strip()
+        # if the last line is not the one we want, get the last 100 lines and search
+        if 'in' not in last_line or "passed" not in last_line:
+            last_lines = subprocess.check_output(
+                ['tail', '-500', filename]).decode('utf-8').strip().split('\n')
+            for line in reversed(last_lines):
+                if 'in' in line and "passed" in line:
+                    last_line = line
+                    return last_line
+        else:
+            return last_line
+    except:
+        return None
 
 
 def get_results(filename, project, algorithm):
