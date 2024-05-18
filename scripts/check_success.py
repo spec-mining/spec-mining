3ae244d8d434038724fbd1bb17da97068381e278
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-05-18 19:04:12 +0300

    add missing dependency of check_success

--- scripts/check_success.py
+++ scripts/check_success.py
@@ -1,11 +1,11 @@
 import os
+import subprocess
 
 def get_result_line(filename):
     # get last line from file
     try:
         last_line = subprocess.check_output(
             ['tail', '-1', filename]).decode('utf-8').strip()
-        print('last line', last_line)
         # if the last line is not the one we want, get the last 100 lines and search
         if 'in' not in last_line or "passed" not in last_line:
             last_lines = subprocess.check_output(
@@ -16,20 +16,21 @@ def get_result_line(filename):
                     return last_line
         else:
             return last_line
-    except:
+    except Exception as e:
+        print('Error: ', e)
         return None
 
 def check_success():
     file_name = os.environ.get('REPORT_FILE')
     result_line = get_result_line(file_name)
 
-    print('Result Line: ', result_line, 'of file', file_name)
+    print('Result Line of file', file_name, 'is: \n', result_line)
 
     if result_line is None:
-        print('exiting with an error')
+        print('Exiting with an error')
         exit(1)
     else:
-        print('exiting with success')
+        print('Exiting with success')
         exit(0)
 
 if __name__ == '__main__':
