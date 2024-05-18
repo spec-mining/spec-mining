8544e280358cc5ab168c79bddb8c83a7c89bc061
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-05-18 15:19:36 +0300

    move function itself

--- scripts/check_success.py
+++ scripts/check_success.py
@@ -1,5 +1,22 @@
 import os
-from parse_reports import get_result_line
+
+def get_result_line(filename):
+    # get last line from file
+    try:
+        last_line = subprocess.check_output(
+            ['tail', '-1', filename]).decode('utf-8').strip()
+        # if the last line is not the one we want, get the last 100 lines and search
+        if 'in' not in last_line or "passed" not in last_line:
+            last_lines = subprocess.check_output(
+                ['tail', '-500', filename]).decode('utf-8').strip().split('\n')
+            for line in reversed(last_lines):
+                if 'in' in line and "passed" in line:
+                    last_line = line
+                    return last_line
+        else:
+            return last_line
+    except:
+        return None
 
 def check_success():
     file_name = os.environ.get('REPORT_FILE')
