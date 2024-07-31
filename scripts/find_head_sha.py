f3515983e9f107add7e2c593eb94b3a0f12ed86a
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-07-31 10:01:08 +0300

    fix: retry only on failure

--- scripts/find_head_sha.py
+++ scripts/find_head_sha.py
@@ -7,17 +7,25 @@ def authenticate_gsheets():
     gc = pygsheets.authorize(service_account_env_var='GOOGLE_CREDENTIALS')
     return gc
 
-def get_github_sha(repo_link):
-    try:
-        api_url = f"https://api.github.com/repos/{repo_link.split('github.com/')[1]}/commits"
-        response = requests.get(api_url)
-        response.raise_for_status()  # Raise an error for bad responses
-        sha = response.json()[0]['sha']
-        print(f'Got SHA for {repo_link}:{sha}')
-        return sha
-    except requests.RequestException as e:
-        print(f"Failed to fetch SHA fo value {repo_link}: {e}")
-        return None
+def get_github_sha(repo_link, retries=15, wait=15):
+    try_count = 0
+    while try_count < retries:
+        try:
+            api_url = f"https://api.github.com/repos/{repo_link.split('github.com/')[1]}/commits"
+            response = requests.get(api_url)
+            response.raise_for_status()  # Raise an error for bad responses
+            sha = response.json()[0]['sha']
+            return sha
+        except requests.RequestException as e:
+            if response.status_code == 403 and 'rate limit exceeded' in str(e).lower():
+                print(f"Rate limit exceeded for {repo_link}. Retrying in {wait} seconds...")
+                time.sleep(wait)
+                try_count += 1
+            else:
+                print(f"Failed to fetch SHA for {repo_link}: {e}")
+                return None
+    print(f"Exceeded maximum retries for {repo_link}")
+    return None
 
 def update_google_sheet_in_bulk(gc, sheet_id, sha_values):
     sheet = gc.open_by_key(sheet_id)
@@ -39,7 +47,6 @@ def main():
     for repo_link in repo_links:
         print(f'Processing {cur_repo_index}/{repo_count}')
         sha_values.append(get_github_sha(repo_link))
-        time.sleep(0.7)  # Wait for 700 milliseconds
         cur_repo_index = cur_repo_index + 1
 
     update_google_sheet_in_bulk(gc, sheet_id, sha_values)
