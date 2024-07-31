7b1e33bfbe5f4378d7b6741b24ae48389f72bf13
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-07-31 11:17:09 +0300

    fix: switch to graphql for better rate limit

--- scripts/find_head_sha.py
+++ scripts/find_head_sha.py
@@ -7,26 +7,64 @@ def authenticate_gsheets():
     gc = pygsheets.authorize(service_account_env_var='GOOGLE_CREDENTIALS')
     return gc
 
-def get_github_sha(repo_link, retries=15, wait=30):
-    try_count = 0
-    while try_count < retries:
-        try:
-            api_url = f"https://api.github.com/repos/{repo_link.split('github.com/')[1]}/commits"
-            response = requests.get(api_url)
-            response.raise_for_status()  # Raise an error for bad responses
-            sha = response.json()[0]['sha']
-            print(f'Fetched SHA for {api_url}:{sha}')
-            return sha
-        except requests.RequestException as e:
-            if response.status_code == 403 and 'rate limit exceeded' in str(e).lower():
-                print(f"Rate limit exceeded for {repo_link}. Retrying in {wait} seconds...")
-                time.sleep(wait)
-                try_count += 1
-            else:
-                print(f"Failed to fetch SHA for {repo_link}: {e}")
-                return None
-    print(f"Exceeded maximum retries for {repo_link}")
-    return None
+def get_github_shas(repo_links, batch_size=50, retries=15, wait=30):
+    github_token = os.getenv('GITHUB_TOKEN')
+    headers = {'Authorization': f'bearer {github_token}'}
+    query = """
+    query($repos: [ID!]!) {
+      nodes(ids: $repos) {
+        ... on Repository {
+          nameWithOwner
+          defaultBranchRef {
+            target {
+              ... on Commit {
+                oid
+              }
+            }
+          }
+        }
+      }
+    }
+    """
+
+    shas = {}
+    for i in range(0, len(repo_links), batch_size):
+        batch = repo_links[i:i + batch_size]
+        repo_ids = [f"repo:{link.split('github.com/')[1]}" for link in batch]
+        variables = {"repos": repo_ids}
+
+        print(f'Processing batch {i}/{len(repo_links)/batch_size}...')
+
+        try_count = 0
+        while try_count < retries:
+            try:
+                response = requests.post(
+                    'https://api.github.com/graphql',
+                    json={'query': query, 'variables': variables},
+                    headers=headers
+                )
+                response.raise_for_status()
+
+                data = response.json()
+                for node in data['data']['nodes']:
+                    if node:
+                        repo_name = node['nameWithOwner']
+                        sha = node['defaultBranchRef']['target']['oid']
+                        shas[repo_name] = sha
+
+                break  # Break the retry loop if the request is successful
+            except requests.RequestException as e:
+                if response.status_code == 403 and 'rate limit exceeded' in str(e).lower():
+                    print(f"Rate limit exceeded. Retrying in {wait} seconds...")
+                    time.sleep(wait)
+                    try_count += 1
+                else:
+                    print(f"Failed to fetch SHAs for batch {i // batch_size + 1}: {e}")
+                    break
+        
+        print(f'Batch {i} response', response)
+
+    return shas
 
 def update_google_sheet_in_bulk(gc, sheet_id, sha_values):
     sheet = gc.open_by_key(sheet_id)
@@ -41,15 +79,9 @@ def main():
     wks = sheet.sheet1
     
     repo_links = wks.get_col(1, include_tailing_empty=False)
-    repo_count = len(repo_links)
+    shas = get_github_shas(repo_links)
 
-    sha_values = []
-    cur_repo_index = 1
-    for repo_link in repo_links:
-        print(f'Processing {cur_repo_index}/{repo_count}')
-        sha_values.append(get_github_sha(repo_link))
-        cur_repo_index = cur_repo_index + 1
-
+    sha_values = [shas.get(link.split('github.com/')[1], None) for link in repo_links]
     update_google_sheet_in_bulk(gc, sheet_id, sha_values)
 
 if __name__ == "__main__":
