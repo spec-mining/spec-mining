27cb2ce7e94bf7f9cf07366c0d75279560016a50
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-09-06 14:01:31 +0300

    largen experiment

--- time-experiment/time-experiment.sh
+++ time-experiment/time-experiment.sh
@@ -2,10 +2,10 @@
 
 # Define the ranges for algo, instance_count, event_count, and raises_violation
 algorithms=("A" "B" "C" "C+" "D")
-# instance_counts=(1 100 500 1000 5000)
-instance_counts=(1 100)
-# event_counts=(5 500 2500 5000 25000)
-event_counts=(5 500)
+instance_counts=(1 100 500 1000 5000)
+# instance_counts=(1 100)
+event_counts=(5 500 2500 5000 25000)
+# event_counts=(5 500)
 violations=("True" "False")
 
 # Create the CSV file and write the header row
