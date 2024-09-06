73839bc8e0cb89cea7a1acfe5c1b6477750afb43
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-09-06 18:14:47 +0300

    denini changes

--- time-experiment/time-experiment.sh
+++ time-experiment/time-experiment.sh
@@ -1,47 +1,36 @@
 #!/bin/bash
 
 # Define the ranges for algo, instance_count, event_count, and raises_violation
-algorithms=("A" "B" "C" "C+" "D")
-# algorithms=("D" "C+" "C" "B" "A")
-instance_counts=(1 100 500)
-# instance_counts=(1 100)
-event_counts=(5 500 2500 5000 25000)
-# event_counts=(5 500)
+algorithms=("D" "C+" "C" "B") # removed "A" because I want to test the other algorithms
+instance_counts=(1 25 50 75 100)
+event_counts=(5 100 500 750 1000)
 violations=("True" "False")
 
 # Create the CSV file and write the header row
 output_file="time-experiment-results.csv"
-header="algo"
-for instance in "${instance_counts[@]}"; do
-    for event in "${event_counts[@]}"; do
-        for violation in "${violations[@]}"; do
-            header+=",${instance}_${event}_${violation}"
-        done
-    done
-done
+header="algo,instance_count,event_count,raises_violation,time"
+
 echo "$header" > "$output_file"
 
 # Iterate over all algorithms
-for algo in "${algorithms[@]}"; do
-    row="$algo"
-    
+
     # Iterate over all combinations of instance_count, event_count, and raises_violation
-    for instance in "${instance_counts[@]}"; do
-        for event in "${event_counts[@]}"; do
-            for violation in "${violations[@]}"; do
-                
+for instance in "${instance_counts[@]}"; do
+    for event in "${event_counts[@]}"; do
+        for violation in "${violations[@]}"; do
+            for algo in "${algorithms[@]}"; do
+                echo Running: $algo $instance $event $violation
                 # Run the Python script and capture the output
                 output=$(python3 ./fake_spec.py "$algo" "$instance" "$event" "$violation")
                 
                 # Extract the time from the output
                 time=$(echo "$output" | grep -oP 'Time:\s*\K[\d.]+')
+                echo "  Time: $time"
                 
                 # Add the time to the current row
-                row+=",$time"
+                row="$algo,$instance,$event,$violation,$time"
+                echo "$row" >> "$output_file"
             done
         done
-    done
-    
-    # Write the row to the CSV file
-    echo "$row" >> "$output_file"
+    done    
 done
