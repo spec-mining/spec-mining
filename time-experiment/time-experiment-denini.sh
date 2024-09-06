16854a771608b04e3a2b9ed4021ccb523856501a
Denini Gabriel <denini.gabriel@gmail.com>
2024-09-06 11:31:48 -0300

    Add files via upload

---
+++ time-experiment/time-experiment-denini.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define the ranges for algo, instance_count, event_count, and raises_violation
+algorithms=("D" "C+" "C" "B") # removed "A" because I want to test the other algorithms
+instance_counts=(1 25 50 75 100 200)
+event_counts=(5 100 500 750 1000)
+violations=("True" "False")
+
+# Create the CSV file and write the header row
+output_file="time-experiment-results.csv"
+header="algo,instance_count,event_count,raises_violation,time"
+
+echo "$header" > "$output_file"
+
+# Iterate over all algorithms
+for algo in "${algorithms[@]}"; do
+    
+    
+    # Iterate over all combinations of instance_count, event_count, and raises_violation
+    for instance in "${instance_counts[@]}"; do
+        for event in "${event_counts[@]}"; do
+            for violation in "${violations[@]}"; do
+                
+                echo Running: $algo $instance $event $violation
+                # Run the Python script and capture the output
+                output=$(python3 ./fake_spec.py "$algo" "$instance" "$event" "$violation")
+                
+                # Extract the time from the output
+                time=$(echo "$output" | grep -oP 'Time:\s*\K[\d.]+')
+                echo "  Time: $time"
+                
+                # Add the time to the current row
+                row="$algo,$instance,$event,$violation,$time"
+                echo "$row" >> "$output_file"
+            done
+        done
+    done    
+done
