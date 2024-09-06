7adde6ba0dbc39f57af5cd60e70f0e4918ef9d99
Denini Gabriel <denini.gabriel@gmail.com>
2024-09-06 11:40:48 -0300

    Update time-experiment-denini.sh

--- time-experiment/time-experiment-denini.sh
+++ time-experiment/time-experiment-denini.sh
@@ -2,7 +2,7 @@
 
 # Define the ranges for algo, instance_count, event_count, and raises_violation
 algorithms=("D" "C+" "C" "B") # removed "A" because I want to test the other algorithms
-instance_counts=(1 25 50 75 100 200)
+instance_counts=(1 25 50 75 100)
 event_counts=(5 100 500 750 1000)
 violations=("True" "False")
 
@@ -13,14 +13,12 @@ header="algo,instance_count,event_count,raises_violation,time"
 echo "$header" > "$output_file"
 
 # Iterate over all algorithms
-for algo in "${algorithms[@]}"; do
-    
-    
+
     # Iterate over all combinations of instance_count, event_count, and raises_violation
-    for instance in "${instance_counts[@]}"; do
-        for event in "${event_counts[@]}"; do
-            for violation in "${violations[@]}"; do
-                
+for instance in "${instance_counts[@]}"; do
+    for event in "${event_counts[@]}"; do
+        for violation in "${violations[@]}"; do
+            for algo in "${algorithms[@]}"; do
                 echo Running: $algo $instance $event $violation
                 # Run the Python script and capture the output
                 output=$(python3 ./fake_spec.py "$algo" "$instance" "$event" "$violation")
