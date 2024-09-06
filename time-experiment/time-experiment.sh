066aaf61659818b0ffb4710db032dbf64968d5a4
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-09-06 16:04:28 +0300

    reverse algo order

--- time-experiment/time-experiment.sh
+++ time-experiment/time-experiment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define the ranges for algo, instance_count, event_count, and raises_violation
-# algorithms=("A" "B" "C" "C+" "D")
-algorithms=("D" "C+" "C" "B" "A")
+algorithms=("A" "B" "C" "C+" "D")
+# algorithms=("D" "C+" "C" "B" "A")
 instance_counts=(1 100 500)
 # instance_counts=(1 100)
 event_counts=(5 500 2500 5000 25000)
