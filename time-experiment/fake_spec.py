4381474a2c16288dfeb51e9069993e14e374396b
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-09-07 13:24:06 +0300

    update experiment with denini changes

--- time-experiment/fake_spec.py
+++ time-experiment/fake_spec.py
@@ -1,17 +1,30 @@
 # ============================== Define spec ==============================
-from pythonmop import Spec, call
 import builtins
 import time
+import random
+import cProfile
 import argparse
+from pythonmop import Spec, call, TRUE_EVENT, FALSE_EVENT
+from string import Template
+import pythonmop.spec.spec as spec
+
+spec.DONT_MONITOR_PYTHONMOP = False
+random.seed(99)
 
 parser = argparse.ArgumentParser("Fakse Program")
 parser.add_argument("algo", help="The algorithm to run", type=str, choices=['A', 'B', 'C', 'C+', 'D'])
 parser.add_argument("instance_count", help="The number of instances", type=int)
 parser.add_argument("event_count", help="The number of events", type=int)
-parser.add_argument("raises_violation", help="Whether or not it should raise a violation", type=bool)
 args = parser.parse_args()
 
-# take algo as an argument when calling the file from command line
+# Replace the argparse section with this:
+# class FakeArgs:
+#     def __init__(self):
+#         self.algo = 'C+'
+#         self.instance_count = 100
+#         self.event_count = 1000
+
+# args = FakeArgs()
 
 original_print = builtins.print
 def mock_print():
@@ -23,112 +36,82 @@ def mock_print():
 def unmock_print(*args, **kwargs):
     builtins.print = original_print
 
-
-class A:
-    def a(self):
-        pass
-
-    def b(self):
-        pass
-
-    def c(self):
-        pass
-
-    def d(self):
-        pass
-
-    def e(self):
-        pass
-
-class FakeSpec(Spec):
+class StringTemplate_ChangeAfterCreate(Spec):
     """
-        must call methods in order a, b, c, d, e (order of c and d doesn't matter)
+    Note further that you cannot change the delimiter after class creation
+    (i.e. a different delimiter must be set in the subclass’s class namespace).
+    src: https://docs.python.org/3/library/string.html#string.Template.template
     """
+
     def __init__(self):
+        print('--- string template change after create ---')
         super().__init__()
+        self.created_classes_delimier = {}  # key: class, value: delimiter
 
-        @self.event_before(call(A, 'a'))
-        def a(**kw):
-            pass
+        @self.event_before(call(Template, '__init__'))
+        def class_creation(**kw):
+            obj = kw['obj']
+            self.created_classes_delimier[obj] = obj.delimiter
 
-        @self.event_before(call(A, 'b'))
-        def b(**kw):
-            pass
-    
-        @self.event_before(call(A, 'c'))
-        def c(**kw):
-            pass
+        @self.event_before(call(Template, 'substitute'))
+        def call_substitute(**kw):
+            return check_change(**kw)
 
-        @self.event_before(call(A, 'd'))
-        def d(**kw):
-            pass
+        @self.event_before(call(Template, 'safe_substitute'))
+        def call_safe_substitute(**kw):
+            return check_change(**kw)
 
-        @self.event_before(call(A, 'e'))
-        def e(**kw):
-            pass
+        def check_change(**kw):
+            obj = kw['obj']
+            if obj in self.created_classes_delimier:
+                if obj.delimiter != self.created_classes_delimier[obj]:
+                    return TRUE_EVENT
 
-    fsm = """
-        s0 [
-            a -> s1
-            default s6
-        ]
-        s1 [
-            b -> s2
-            default s6
-        ]
-        s2 [
-            c -> s3
-            d -> s3
-            default s6
-        ]
-        s3 [
-            c -> s4
-            d -> s4
-            default s6
-        ]
-        s4 [
-            e -> s5
-            default s6
-        ]
-        s5 [
-            default s5
-        ]
-        s6 [
-            default s6
-        ]
-        alias match = s6
-    """
-    creation_events = ['a']
+            return FALSE_EVENT
+
+    ere = 'class_creation (call_safe_substitute | call_substitute)+'
+    creation_events = ['class_creation']
 
     def match(self, call_file_name, call_line_num):
         print(
-            f'Spec - {self.__class__.__name__}: Should call in order.'
+            f'Spec - {self.__class__.__name__}: Note further that you cannot change the delimiter after class creation.'
             f'file {call_file_name}, line {call_line_num}.')
 
 
-# =========================================================================
-def execute_fake_program(instance_count, event_count, raises_violation):
-    events = ['a', 'b', 'c', 'e', 'd'] if raises_violation else ['a', 'b', 'c', 'd', 'e']
+class MyTemplate(Template):
+    delimiter = '%'
 
+
+def execute_fake_program(instance_count, event_count):
+    # create a spec
+    string_template_instance = StringTemplate_ChangeAfterCreate()
+    string_template_instance.create_monitor(args.algo)
+
+    # create instances
+    all_instances = []
+    all_possible_events = ['substitute', 'safe_substitute']
     for i in range(instance_count):
-        instance = A()
-        for e in range(event_count):
-            event_name = events[i % 5]
-            getattr(instance, event_name)()
+        all_instances.append(MyTemplate('Hello'))
+    for i in range(event_count):
+        inst = random.choice(all_instances)
+        event = random.choice(all_possible_events)
+        # 10 % of the time, we will violate the spec
+        violate = random.random() < 0.1
+        if violate:
+            inst.delimiter = '#'
+        getattr(inst, event)(who='world')
+    if args.algo == 'A':
+        string_template_instance.get_monitor().refresh_monitor()
+
 
 mock_print()
-
-spec_instance = FakeSpec()
-spec_instance.create_monitor(args.algo)
-
 time_1 = time.time()
-execute_fake_program(args.instance_count, args.event_count, raises_violation=args.raises_violation)
+# cProfile.run('execute_fake_program(args.instance_count, args.event_count)', sort='cumulative')
+execute_fake_program(args.instance_count, args.event_count)
 time_2 = time.time()
-
 unmock_print()
 
 print('Algo: ', args.algo)
 print('Instance Count: ', args.instance_count)
 print('Event Count: ', args.event_count)
-print('Raises a violation: ', args.raises_violation)
 print('Time:', time_2 - time_1)
--- time-experiment/time-experiment.sh
+++ time-experiment/time-experiment.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
-# Define the ranges for algo, instance_count, event_count, and raises_violation
-algorithms=("D" "C+" "C" "B") # removed "A" because I want to test the other algorithms
-instance_counts=(1 25 50 75 100)
+# Define the ranges for algo, instance_count, and event_count
+algorithms=("A" "B" "C" "C+" "D")
+instance_counts=(1 50 100 200 400)
 event_counts=(5 100 500 750 1000)
-violations=("True" "False")
 
 # Create the CSV file and write the header row
 output_file="time-experiment-results.csv"
-header="algo,instance_count,event_count,raises_violation,time"
+header="algo,instance_count,event_count,time"
 
 echo "$header" > "$output_file"
 
-# Iterate over all algorithms
-
-    # Iterate over all combinations of instance_count, event_count, and raises_violation
+# Iterate over all combinations of instance_count, event_count, and algo
 for instance in "${instance_counts[@]}"; do
     for event in "${event_counts[@]}"; do
-        for violation in "${violations[@]}"; do
-            for algo in "${algorithms[@]}"; do
-                echo Running: $algo $instance $event $violation
-                # Run the Python script and capture the output
-                output=$(python3 ./fake_spec.py "$algo" "$instance" "$event" "$violation")
-                
-                # Extract the time from the output
-                time=$(echo "$output" | grep -oP 'Time:\s*\K[\d.]+')
-                echo "  Time: $time"
-                
-                # Add the time to the current row
-                row="$algo,$instance,$event,$violation,$time"
-                echo "$row" >> "$output_file"
-            done
+        for algo in "${algorithms[@]}"; do
+            echo Running: $algo $instance $event
+            # Run the Python script and capture the output
+            output=$(python3 ./fake_spec.py "$algo" "$instance" "$event")
+            
+            # Extract the time from the output
+            time=$(echo "$output" | grep -oP 'Time:\s*\K[\d.]+')
+            echo "  Time: $time"
+            
+            # Add the time to the current row
+            row="$algo,$instance,$event,$time"
+            echo "$row" >> "$output_file"
         done
     done    
 done
