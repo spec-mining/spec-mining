e7a2e77dd4516b83e771cec36a29b879e84d9209
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2024-10-28 17:57:56 +0300

    remove some dependencies to test

--- scripts/install.sh
+++ scripts/install.sh
@@ -59,9 +59,6 @@ if [ "$ALGO" != "ORIGINAL" ]; then
     sudo apt-get install python3-tk -y
 else
     pip3 install pytest-json-report memray pytest-memray pytest-cov pytest-env pytest-rerunfailures pytest-socket pytest-django
-
-    # Install additional dependencies that help projects run
-    pip3 install absl-py==2.1.0 astunparse==1.6.3 blinker==1.8.2 forbiddenfruit==0.1.4 gast==0.6.0 google-pasta==0.2.0 grpcio==1.67.0 h5py==3.12.1 JPype1==1.5.0 keras==3.6.0 libclang==18.1.1 Markdown==3.7 markdown-it-py==3.0.0 mdurl==0.1.2 ml-dtypes==0.4.1 namex==0.0.8 nltk==3.9.1 numpy==2.0.2 opt_einsum==3.4.0 optree==0.13.0 packaging==24.1 protobuf==5.28.3 Pygments==2.18.0 regex==2024.9.11 rich==13.9.3 scipy==1.14.1 tensorboard==2.18.0 tensorboard-data-server==0.7.2 tensorflow==2.18.0 termcolor==2.5.0 tk==0.1.0 tornado==6.4.1 tqdm==4.66.5 typing_extensions==4.12.2 Werkzeug==3.0.6 wrapt==1.16.0
 fi
 
 cd -
